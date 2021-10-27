b16dd8d8b2d5d80f6a79056c5b60ad4a316bd20c
NicoleAsqui <nasqui@espol.edu.ec>
2021-10-27 17:12:30 -0500

    añadir atributos detallados a solicitud

--- AgenciaLaboral/applications/Solicitud/models.py
+++ AgenciaLaboral/applications/Solicitud/models.py
@@ -14,15 +14,21 @@ class Solicitud(models.Model):
     sueldo = models.FloatField(blank=True, null=True)
     fechainicio = models.DateTimeField(db_column='fechaInicio', blank=True, null=True)  # Field name made lowercase.
     fechacierre = models.DateTimeField(db_column='fechaCierre', blank=True, null=True)  # Field name made lowercase.
-    cargo = models.CharField(max_length=45, blank=True, null=True)
-    descripcioncargo = models.CharField(db_column='descripcionCargo', max_length=200, blank=True, null=True)  # Field name made lowercase.
+    cargo = models.CharField(max_length=100, blank=True, null=True)
+    descripcioncargo = models.TextField(db_column='descripcionCargo', blank=True, null=True)  
     tiposolicitud_idtiposolicitud = models.ForeignKey(Tiposolicitud, models.DO_NOTHING, db_column='TipoSolicitud_idTipoSolicitud', null = True)  # Field name made lowercase.
     estado_idestado = models.ForeignKey(Estado, models.DO_NOTHING, db_column='Estado_idEstado', null = True)  # Field name made lowercase.
     representante_idrepresentante = models.ForeignKey(Representanteempresa, models.DO_NOTHING, db_column='representante_idrepresentante' , null = True)  # Field name made lowercase.
     provincia_idprovincia = models.ForeignKey(Provincia, models.DO_NOTHING, db_column='provincia_idProvincia', null=True)  # Field name made lowercase.
     ciudad_idciudad = models.ForeignKey(Ciudad, models.DO_NOTHING, db_column='ciudad_idCiudad', null=True)  # Field name made lowercase.
-    
-      
+    educacion_minima = models.CharField(max_length=45, blank=True, null=True)
+    jornada = models.CharField(max_length=45, blank=True, null=True)
+    discapacidad = models.CharField(max_length=45, blank=True, null=True)
+    disponibilidad_viajar =   models.CharField(max_length=45, blank=True, null=True)
+    disponibilidad_cambioresidencia =   models.CharField(max_length=45, blank=True, null=True)
+    licencia =   models.CharField(max_length=45, blank=True, null=True)    
+    idiomas =   models.CharField(max_length=45, blank=True, null=True)
+
     def __str__(self):
         txt = " Profesion: {0} / Cargo: {1} "
         return txt.format(self.profesion , self.cargo) 
--- AgenciaLaboral/applications/Solicitud/serializers.py
+++ AgenciaLaboral/applications/Solicitud/serializers.py
@@ -28,6 +28,13 @@ class SolicitudSerializer(serializers.ModelSerializer):
         instance.representante_idrepresentante = validated_data.get('representante_idrepresentante', instance.representante_idrepresentante)
         instance.provincia_idprovincia = validated_data.get('provincia_idprovincia', instance.provincia_idprovincia)
         instance.ciudad_idciudad = validated_data.get('ciudad_idciudad', instance.ciudad_idciudad)
+        instance.educacion_minima = validated_data.get('educacion_minima', instance.ciudad_idciudad)
+        instance.jornada = validated_data.get('jornada', instance.ciudad_idciudad)
+        instance.discapacidad = validated_data.get('discapacidad', instance.ciudad_idciudad)
+        instance.disponibilidad_viajar = validated_data.get('disponibilidad_viajar', instance.ciudad_idciudad)
+        instance.disponibilidad_cambioresidencia = validated_data.get('disponibilidad_cambioresidencia', instance.ciudad_idciudad)
+        instance.licencia = validated_data.get('licencia', instance.ciudad_idciudad)
+        instance.idiomas = validated_data.get('idiomas', instance.ciudad_idciudad)
         
         instance.save()
         return instance
