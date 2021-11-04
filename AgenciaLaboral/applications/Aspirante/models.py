9ff2228676f06c3ea9cc695631496014ee7ce2f7
Jeremy-Ramirez <66746920+Jeremy-Ramirez@users.noreply.github.com>
2021-11-04 18:23:43 -0500

    agregando idiomas

--- AgenciaLaboral/applications/Aspirante/models.py
+++ AgenciaLaboral/applications/Aspirante/models.py
@@ -20,6 +20,7 @@ class Aspirante(models.Model):
     posibilidadcambioresidencia = models.CharField(max_length=5,db_column='posibilidadCambioResidencia', blank=True, null=True)  # Field name made lowercase.
     estadoestudios = models.CharField(max_length=5,db_column='estadoEstudios', blank=True, null=True)  # Field name made lowercase.
     profesiones_idprofesiones = models.ForeignKey(Profesiones, models.DO_NOTHING, db_column='Profesiones_idProfesiones', null=True)  # Field name made lowercase.
+    idiomas = models.CharField(max_length=500,db_column='idiomas', blank=True, null=True)  # Field name made lowercase.
     nivelestudios_idnivelestudios = models.ForeignKey(NivelEstudios, models.DO_NOTHING, db_column='NivelEstudios_idNivelEstudios', null=True)  # Field name made lowercase.
     usuario_idusuario = models.ForeignKey(Usuario, models.DO_NOTHING, db_column='usuario_idusuario',null=True) # Field name made lowercase.
     
